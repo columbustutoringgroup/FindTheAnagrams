653b03fb36b31a741736451e65a0932ebbe16bb9
Nick Legg <nlegg@covermymeds.com>
2015-10-16 15:44:01 -0400

    Return just one word instead of the whole array for later comparison

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,6 +1,6 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
 def canonical(word)
-  word.downcase.chars.permutation.map{ |word| word.join }.sort
+  word.downcase.chars.permutation.map{ |word| word.join }.sort.first
 end
 
 def detect_anagram(word1, word2)
