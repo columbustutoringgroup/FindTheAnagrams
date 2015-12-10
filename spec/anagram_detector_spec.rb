4320876329e429644a8a97b8b6bdea8dd0fa9abc
Samuel Priddy <spriddy@covermymeds.com>
2015-12-10 15:57:18 -0500

    Move RSpec tests into separate spec file.

---
+++ spec/anagram_detector_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require_relative '../anagram_detector'
+
+
+RSpec.describe 'canonical' do
+  it 'takes a string and returns an alphanumerically sorted version of it' do
+    expected = 'allsy'
+    actual = canonical 'sally'
+    expect(expected).to eq(actual)
+  end
+end
+
+RSpec.describe 'detect_anagram' do
+  it 'outputs true if two words are anagrams (case-insensitive)' do
+    expect(detect_anagram('BeeWizard', 'WideZebra')).to be true
+  end
+  
+  it 'outputs false if two words are not anagrams' do
+    expect(detect_anagram('CoverMyMeds', 'CoverMyLEDs')).to be false
+  end
+end
