7ffd6fcbe5a3b32fbbdcf9199176936120653b4c
Brendan Thomas <brthomas@covermymeds.com>
2016-05-19 16:42:07 -0400

    Solution

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,5 +1,7 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
 def canonical(word)
+	wordArr = word.split(//)
+	wordArr.sort!.each { |x| x.downcase! }
 end
 
 def detect_anagram(word1, word2)
@@ -7,10 +9,9 @@ def detect_anagram(word1, word2)
 end
 
 p "Do NOT change any code below this line"
-p detect_anagram('angel', 'glean') == true
-p detect_anagram('glean', 'angel') == true
-p detect_anagram('pants', 'pants') == true
-p detect_anagram('CinEmA', 'iceman') == true
-p detect_anagram('defgh8', 'g8hefd') == true
-
-
+p detect_anagram('angel', 'glean') #== true
+p detect_anagram('glean', 'angel') #== true
+p detect_anagram('pants', 'pants') #== true
+p detect_anagram('CinEmA', 'iceman') #== true
+p detect_anagram('defgh8', 'g8hefd') #== true
+p detect_anagram('bird', 'house') #should == false
