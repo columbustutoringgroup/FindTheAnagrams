fdd96f635bff7fc80222186ee18262de8cc27b0d
Entela Duka <enteladuka@gmail.com>
2018-11-04 19:53:27 -0500

    Anagram Challenge Complete

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,5 +1,11 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
+
+# Canonical function
+# sorts string into an array of characters in alphabetical order, makes them lowercase, and rejoins them back into an array
+# if two words are anagrams of one another, the canonical function should result in the same word 
+
 def canonical(word)
+  word.chars.sort.join.downcase
 end
 
 def detect_anagram(word1, word2)
@@ -12,5 +18,3 @@ p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
-
