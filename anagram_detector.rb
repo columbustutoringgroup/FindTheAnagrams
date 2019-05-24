8c63613e619bbf5fdb9a30c5ca6db192bece528c
thunter <thunter@covermymeds.com>
2019-05-24 14:46:58 -0400

    anagram solution

--- anagram_detector.rb
+++ anagram_detector.rb
@@ -1,8 +1,10 @@
 # Implement this in such a way that when called below, detect_anagram will result in true or false.
 def canonical(word)
+   puts word.downcase.chars.sort
 end
 
 def detect_anagram(word1, word2)
+
   canonical(word1) == canonical(word2)
 end
 
@@ -12,5 +14,4 @@ p detect_anagram('glean', 'angel') == true
 p detect_anagram('pants', 'pants') == true
 p detect_anagram('CinEmA', 'iceman') == true
 p detect_anagram('defgh8', 'g8hefd') == true
-
-
+p detect_anagram('bird', 'house') == false
